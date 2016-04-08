4d9603cbd64797c3ea10726578c90ae77fd0f941
ravenusmc <mcuddy77@gmail.com>
2016-04-08 11:39:18 -0400

    added a create action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,27 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all.order("created_at DESC")
   end 
 
-  def new 
+  def new
+    #Responsible for the new item 
     @item = Item.new 
   end 
 
   def create
+    #Responsible for saving
+    @item = Item.new(item_params)
+    if @item.save 
+      redirect_to root_path
+    else 
+      render 'new'
+    end 
   end 
 
+  private 
+
+    def item_params 
+      params.require(:item).permit(:title, :description)
+    end 
 end
