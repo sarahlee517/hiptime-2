0c2f8b7cce9098a5c388ac32d4e3743bd5eb9305
ravenusmc <mcuddy77@gmail.com>
2016-04-09 14:21:47 -0400

    added a migration to a table

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,6 +47,12 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end 
 
+  def complete
+    @item = Item.find(params[:id])
+    @item.update_attribute(:completed_at, Time.now)
+    redirect_to root_path
+  end 
+
   private 
 
     def find_item
@@ -57,3 +63,22 @@ class ItemsController < ApplicationController
       params.require(:item).permit(:title, :description)
     end 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
